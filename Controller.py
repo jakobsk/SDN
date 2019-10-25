d128b43a22684c718395bff5ed114107f5763333
jakobsk <jakobskok95@gmail.com>
2019-10-25 11:07:45 +0200

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -2,14 +2,14 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_3
+from ryu.ofproto import ofproto_v1_0
 
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 
 
 class L2Switch(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
 
 
     def __init__(self, *args, **kwargs):
